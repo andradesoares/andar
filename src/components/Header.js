fd5d3c6f958b06fa8fccb100422a099f92a59cbb
Daniel Soares <andrade_soares@live.com>
2023-03-22 23:41:19 -0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+import * as styles from "./Header-Style";
+import LogoInstagram from "../../public/instagram.svg";
+import LogoPinterest from "../../public/pinterest.svg";
+
+const Header = () => {
+  return (
+    <styles.Container>
+      <div>
+        <p>Marina Braga</p>
+        <p>Assitente Virtual</p>
+      </div>
+      <styles.Navigation>
+        <styles.Links>
+          <styles.Link>
+            <a href="#">Ínicio</a>
+          </styles.Link>
+          <styles.Link>
+            <a href="#">Serviços e Preços</a>
+          </styles.Link>
+          <styles.Link>
+            <a href="#">Sobre</a>
+          </styles.Link>
+          <styles.Link>
+            <a href="#">Contato</a>
+          </styles.Link>
+          <div>
+            <a href="#">
+              <Image
+                src={LogoInstagram}
+                alt="Logo Instagram"
+                width={20}
+                height={20}
+              />
+            </a>
+            <a href="#">
+              <Image
+                src={LogoPinterest}
+                alt="Logo Instagram"
+                width={20}
+                height={20}
+              />
+            </a>
+          </div>
+        </styles.Links>
+      </styles.Navigation>
+    </styles.Container>
+  );
+};
+
+export default Header;
