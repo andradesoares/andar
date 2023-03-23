0ac20059b17da004c2cdc63e12ffe92e080b6caf
Daniel Soares <andrade_soares@live.com>
2023-03-22 23:41:30 -0300

    Banner component

---
+++ src/components/home/Banner-Style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import Default from "../../../public/default.png";
+
+export const Container = styled.div`
+  background-image: url(${Default.src});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  padding: 10vw 0 15vw 0;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.2vw;
+  color: white;
+  text-align: center;
+  margin: 0 0 20px 0;
+`;
+
+export const SubTitle = styled.h3`
+  color: white;
+  text-align: center;
+  margin: 0 0 20px 0;
+`;
+
+export const Btn = styled.div`
+  margin: 0 auto;
+  button {
+    font-family: "Questrial";
+    padding: 0.5vw 1vw;
+  }
+`;
---
+++ src/components/home/Banner.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+import * as styles from "./Banner-Style";
+import Default from "../../../public/default.png";
+
+const Banner = () => {
+  return (
+    <styles.Container>
+      <styles.Title>
+        Lorem ipsum
+        <br /> dolor sit amet.
+      </styles.Title>
+      <styles.SubTitle>
+        Lorem ipsum dolor sit amet, consectetur.
+      </styles.SubTitle>
+      <styles.Btn>
+        <button>Ver Serviços</button>
+      </styles.Btn>
+    </styles.Container>
+  );
+};
+
+export default Banner;
